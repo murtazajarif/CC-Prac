bdbffb83bfe8121c60dbce7c120977838eb047c7
Murtaza Jarif <97272907+murtazajarif@users.noreply.github.com>
2023-11-27 23:18:47 +0530

    Update AWS.py

--- AWS.py
+++ AWS.py
@@ -32,13 +32,22 @@ else:
 
 #Factorial
 
-def factorial_(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
+# Python program to find the factorial of a number provided by the user.
 
-# Example usage:
-number = 5
-result = factorial_(number)
-printf("The factorial of {number} is: {result}")
+# change the value for a different result
+num = 7
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
